50eb49fc4ad38578dc8e9a46b5b4e9a6e559a8d1
mess1ah <463215695@qq.com>
2017-04-15 19:10:32 +0800

    solve find Median Sorted Arrays (H)

---
+++ src/findMedianSortedArrays.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Messiah_S on 15/04/2017.
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = module.exports = function (nums1, nums2) {
+  return findMedian(merge(nums1, nums2));
+};
+
+function merge(nums1, nums2) {
+  var i = 0;
+  var j = 0;
+  var result = [];
+  var nums1Length = nums1.length;
+  var nums2Length = nums2.length;
+  if (nums1Length === 0) {
+    return nums2;
+  }
+  if (nums2Length === 0) {
+    return nums1;
+  }
+  while (i !== nums1Length && j !== nums2Length) {
+    if (nums1[i] < nums2[j]) {
+      result.push(nums1[i]);
+      i = i + 1;
+    } else {
+      result.push(nums2[j]);
+      j = j + 1;
+    }
+  }
+  if (i === nums1Length) {
+    result = result.concat(nums2.slice(j));
+  } else if (j === nums2Length) {
+    result = result.concat(nums1.slice(i));
+  }
+  return result;
+}
+
+function findMedian(array) {
+  var arrayLength = array.length;
+  var medianIndex = (arrayLength - 1) / 2;
+  var floor = Math.floor(medianIndex);
+  if (floor === medianIndex) {
+    return array[medianIndex];
+  }
+  return (array[floor] + array[floor + 1]) / 2;
+}
